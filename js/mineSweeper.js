b4f910005ee86e0d6cca877300f7bb0bf1353142
AkivaAbramson <135149045+AkivaAbramson@users.noreply.github.com>
2023-05-31 19:00:18 +0300

    first-upload

---
+++ js/mineSweeper.js
@@ -0,0 +1,112 @@
+'use strict'
+
+var gBoard
+const gFlag = '🚩'
+var gContent = getContent()
+var gCount = 0
+
+function onInit() {
+    //console.log('hi')
+    gBoard = buildBoard(5)
+    renderBoard(gBoard)
+    
+}
+
+function buildBoard(boardSize) {
+    var board = []
+    for (var i = 0; i < boardSize; i++) {
+        board[i] = []
+        for (var j = 0; j < boardSize; j++) {
+            var currCell = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: false,
+                content: gContent.pop(),
+                flag: '🚩'
+            }
+            board[i][j] = currCell
+        }
+    }
+    for (var i = 0; i < boardSize; i++) {
+        for (var j = 0; j < boardSize; j++) {
+            // debugger
+            var currCell = board[i][j]
+            if (currCell.content === '') {
+                currCell.content = countNegs(board, i, j)
+                if (currCell.content === 0) {
+                    currCell.content = ''
+                }
+
+            }
+        }
+    }
+
+
+    console.table(board)
+    return board
+}
+
+function renderBoard(board) {
+    var strHTML = ''
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+        for (var j = 0; j < board.length; j++) {
+            var cell = board[i][j].content
+            var className = `cell cell-${i}-${j}`
+            strHTML += `
+   <td onclick="onCellClicked(${i},${j})" oncontextmenu="onCellMarked(event,${i},${j})" class="${className}">${cell}</td>`
+        }
+        strHTML += '</tr>'
+    }
+
+    //console.log('strHTML:', strHTML)
+    document.querySelector('.board-container').innerHTML = strHTML
+}
+
+function onCellClicked(i, j) {
+    if(gBoard[i][j].isMarked) return
+    var elCell = document.querySelector(`.cell-${i}-${j}`)
+    elCell.style.backgroundColor = 'gray'
+    gBoard[i][j].isShown = true
+    gCount++
+    if(gBoard[i][j].content === '*'){
+        alert('you lose!')
+    }
+    checkGameOver()
+
+
+
+    console.log(i, j)
+
+}
+
+function onCellMarked(ev, i, j) {
+    ev.preventDefault()
+    var elCell = document.querySelector(`.cell-${i}-${j}`)
+    if(gBoard[i][j].isShown) return
+    if(!gBoard[i][j].isMarked){
+        elCell.innerHTML = gFlag
+        gBoard[i][j].isMarked = true  
+    } else{
+        elCell.innerHTML = gBoard[i][j].content
+        gBoard[i][j].isMarked = false 
+    }
+    
+    
+    console.log(gBoard)
+    console.log(elCell.innerHTML)
+
+}
+
+function checkGameOver(){
+    if(gCount === (5**2)-8){
+        alert('you won!')
+    }
+
+
+}
+
+
+
+
---
+++ js/utils.js
@@ -0,0 +1,28 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive
+}
+
+function countNegs(mat, rowIdx, colIdx) {
+    var count = 0
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i > mat.length - 1) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (j < 0 || j > mat[0].length - 1) continue
+            if (i === rowIdx && j === colIdx) continue
+            var currCell = mat[i][j]
+            if (currCell.content === '*') count++
+        }
+    }
+    return count
+}
+function getContent(){
+    var contentArr = []
+    for(var i = 0;i<8;i++) contentArr.push('*')
+    for(var i = 0;i<17;i++) contentArr.push('')
+   // console.log(contentArr)
+    return contentArr
+}
+
+
